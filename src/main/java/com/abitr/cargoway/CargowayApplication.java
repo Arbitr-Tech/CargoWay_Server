93c60bcb3d23116c04dd73c0b075e9804f2343e2
SagidKamilov <89615854074a@gmail.com>
2025-01-11 13:21:13 +0300

    инициализационные файлы

---
+++ src/main/java/com/abitr/cargoway/CargowayApplication.java
@@ -0,0 +1,13 @@
+package com.abitr.cargoway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargowayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargowayApplication.class, args);
+	}
+
+}
