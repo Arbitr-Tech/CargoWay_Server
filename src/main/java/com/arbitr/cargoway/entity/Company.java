538f211037de49804a7a1ab6037a56aeb7151dca
SagidKamilov <89615854074a@gmail.com>
2025-02-24 20:51:22 +0300

    Добавил аннотации для валидации специальных полей

--- src/main/java/com/arbitr/cargoway/entity/Company.java
+++ src/main/java/com/arbitr/cargoway/entity/Company.java
@@ -1,6 +1,8 @@
 package com.arbitr.cargoway.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,18 +26,23 @@ public class Company {
     @Column(name = "name", nullable = false, unique = false)
     private String name;
 
+    @Pattern(regexp = "\\d{10}|\\d{12}", message = "ИНН должен состоять из 10 или 12 цифр")
     @Column(name = "inn", nullable = false, unique = true, length = 12)
     private String inn;
 
+    @Pattern(regexp = "\\d{13}", message = "ОГРН должен состоять из 13 цифр")
     @Column(name = "ogrn", nullable = false, unique = true, length = 13)
     private String ogrn;
 
+    @Pattern(regexp = "\\d{9}", message = "БИК должен состоять из 9 цифр")
     @Column(name = "bic", nullable = false, unique = false, length = 9)
     private String bic;
 
+    @Pattern(regexp = "\\d{20}", message = "Корреспондентский счёт должен состоять из 20 цифр")
     @Column(name = "correspondent_account", nullable = false, unique = true, length = 20)
     private String correspondentAccount;
 
+    @PastOrPresent
     @Column(name = "registration_date", nullable = false)
     private LocalDate registrationDate;
 
