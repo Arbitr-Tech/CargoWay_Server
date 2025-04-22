43336894cbcba522966e8cab95d1015be27cde5d
SagidKamilov <89615854074a@gmail.com>
2025-04-22 18:59:02 +0300

    #48916119. Заменил возможность поиска от 0 страницы на минимум от 1 страницы

--- src/main/java/com/arbitr/cargoway/dto/rq/PaginationRq.java
+++ src/main/java/com/arbitr/cargoway/dto/rq/PaginationRq.java
@@ -5,9 +5,13 @@ import lombok.Data;
 
 @Data
 public class PaginationRq {
-    @Min(0)
+    @Min(1)
     private int pageNumber;
 
     @Min(1)
     private int pageSize;
+
+    public int getPageNumber() {
+        return pageNumber - 1;
+    }
 }
