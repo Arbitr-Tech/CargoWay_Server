da8a2d29a1371c3584e686692dcf7b4b9a5f3195
SagidKamilov <89615854074a@gmail.com>
2025-05-13 02:11:49 +0300

    #49712335. Удалил лишний функционал

--- src/main/java/com/arbitr/cargoway/mapper/GeneralMapper.java
+++ src/main/java/com/arbitr/cargoway/mapper/GeneralMapper.java
@@ -1,9 +1,7 @@
 package com.arbitr.cargoway.mapper;
 
-import com.arbitr.cargoway.dto.rq.cargo.RecordStatus;
 import com.arbitr.cargoway.dto.rs.ImageRef;
 import com.arbitr.cargoway.entity.Image;
-import com.arbitr.cargoway.entity.enums.CargoOrderStatus;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 
@@ -12,16 +10,6 @@ import java.util.List;
 
 @Mapper
 public interface GeneralMapper {
-    @Named("mapStatusToVisibility")
-    default CargoOrderStatus mapStatusToVisibility(RecordStatus recordStatus) {
-        return CargoOrderStatus.valueOf(recordStatus.name());
-    }
-
-    @Named("mapVisibilityToStatus")
-    default RecordStatus mapStatusToVisibility(CargoOrderStatus transportStatus) {
-        return RecordStatus.valueOf(transportStatus.name());
-    }
-
     @Named("mapImagesToImageRef")
     default List<ImageRef> mapImagesToImageRef(List<Image> images) {
         if (images == null || images.isEmpty()) {
