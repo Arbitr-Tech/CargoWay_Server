3e66730bffbd9ffec7cd71e69a5f67e2970a4dff
SagidKamilov <89615854074a@gmail.com>
2025-05-15 15:29:08 +0300

    #49898092. Исправил баг преобразования имени комментатора при маппинге отзыва

--- src/main/java/com/arbitr/cargoway/mapper/ReviewMapper.java
+++ src/main/java/com/arbitr/cargoway/mapper/ReviewMapper.java
@@ -4,9 +4,11 @@ import com.arbitr.cargoway.dto.general.ReviewDto;
 import com.arbitr.cargoway.dto.rs.review.ReviewRs;
 import com.arbitr.cargoway.entity.Review;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {ProfileMapper.class})
 public interface ReviewMapper {
+    @Mapping(source = "commentator", target = "commentator", qualifiedByName = "toProfileShortDto")
     ReviewRs toRsDto(Review review);
     Review toEntity(ReviewDto reviewDto);
 }
