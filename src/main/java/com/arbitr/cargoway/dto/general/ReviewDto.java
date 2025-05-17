1dfc8ab8b40d81e7087307ca8e0a17069362540f
SagidKamilov <89615854074a@gmail.com>
2025-05-17 13:01:43 +0300

    #49898092. Добавил валидацию в общую дто рейтинга

--- src/main/java/com/arbitr/cargoway/dto/general/ReviewDto.java
+++ src/main/java/com/arbitr/cargoway/dto/general/ReviewDto.java
@@ -1,6 +1,8 @@
 package com.arbitr.cargoway.dto.general;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ public class ReviewDto {
     @Schema(description = "Комментарий отзыва")
     private String comment;
 
+    @Max(value = 5, message = "Рейтинг не может быть больше 5")
+    @Min(value = 1, message = "Рейтинг не может быть меньше 1")
     @Schema(description = "Рейтинг отзыва")
     private Integer rating;
 }
